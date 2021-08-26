83c4b718661207134d26c25b3abd101b31b8bfdf
J. Dylan White <jdwhite5@ncsu.edu>
2021-08-26 14:08:10 -0400

    added daily download functions

--- olr/olr.py
+++ olr/olr.py
@@ -48,6 +48,60 @@ def download_monthly_file(dataDir):
     if not os.path.exists(filePath):
         urllib.request.urlretrieve(dataUrl,filePath)
 
+def get_daily_file_paths(yearList):
+
+    """Find the files, separated by year, for daily OLR data."""
+
+    # Set the base URL for the directory to parse files
+    baseUrl = "https://www.ncei.noaa.gov/data/"+\
+              "outgoing-longwave-radiation-daily/access/"
+
+    # Set the file extension to find
+    ext = ".nc"
+
+    # Get the data from the base URL
+    page = requests.get(baseUrl).text
+
+    # Parse the page results
+    soup = BeautifulSoup(page, 'html.parser')
+
+    # Find all of the hyperlinked anchors on the page
+    fileList = [node.get('href') for node in soup.find_all('a')
+                if node.get('href').endswith(ext)]
+
+    # Create an empty list
+    fileListSubset = []
+
+    # Find the file names that are within the date range
+    for year in yearList:
+
+        # Set the strings to match
+        dateString = f'{year}0101_{year}'
+
+        # Find the file that matches the year
+        fileListSubset.append([baseUrl+fileName for fileName in fileList 
+                               if dateString in fileName][0])
+
+    return fileListSubset
+      
+
+def download_daily_files(dataDir,yearList):
+
+    # Get the list of URLs
+    dataUrlList = get_daily_file_paths(yearList)
+
+    # Loop through the URLs
+    for dataUrl in dataUrlList:
+    
+        # Grab the file name
+        fileName = os.path.basename(dataUrl)
+        filePath = os.path.join(dataDir,fileName)
+
+        # Check if the file exists, and if it doesn't, download it
+        if not os.path.exists(filePath):
+            urllib.request.urlretrieve(dataUrl,filePath)
+
+
 if __name__ == "__main__":
 
     # Set the data directory
@@ -55,3 +109,11 @@ if __name__ == "__main__":
 
     # Download the monthly OLR data
     download_monthly_file(dataDir) 
+
+    # Set the range of years to download daily data
+    beginYear = 2017
+    endYear = 2021
+    yearList = [year for year in range(beginYear,endYear+1)]
+
+    # Download the daily OLR data
+    download_daily_files(dataDir,yearList)
